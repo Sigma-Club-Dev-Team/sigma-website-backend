aa37323681c53f9d867d22309d5fed94e68b8053
Korede <adeyanju.akorede1@gmail.com>
2024-05-19 16:21:48 +0100

    chore: setup logging

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import EnvVars from './constants/EnvVars';
+import { useRequestLogging } from './middlewares/request_logging.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(EnvVars.BASE_URL);
-  await app.listen(EnvVars.Port);
+  useRequestLogging(app);
+  await app.listen(EnvVars.Port, () => {console.log(`Server started & Listening on Port: ${EnvVars.Port}`);});
 }
 bootstrap();
