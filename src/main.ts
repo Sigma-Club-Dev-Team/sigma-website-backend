7203d3ffd92abe92b1be34a832edbaf4c7375c41
Korede <adeyanju.akorede1@gmail.com>
2024-05-19 23:49:50 +0100

    chore: setup validation middleware

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import EnvVars from './constants/EnvVars';
 import { useRequestLogging } from './middlewares/request_logging.middleware';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-   app.use(helmet());
+  useRequestLogging(app);
+  app.use(helmet());
    app.enableCors();
   app.setGlobalPrefix(EnvVars.BASE_URL);
-  useRequestLogging(app);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   await app.listen(EnvVars.Port, () => {console.log(`Server started & Listening on Port: ${EnvVars.Port}`);});
 }
 bootstrap();
