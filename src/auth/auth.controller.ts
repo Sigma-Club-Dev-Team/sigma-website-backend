71630d3b7b9c55c0ac214bdcfed2b19e2bd85731
Korede <adeyanju.akorede1@gmail.com>
2024-05-25 04:45:17 +0100

    fix: only super admin should be able to create account

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,13 +6,17 @@ import { LocalAuthGuard } from './guards/local-auth.guard';
 import { LoginDto } from './dto/login.dto';
 import { RequestWithUser } from './interfaces/request-with-user.interface';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import RolesGuard from './guards/role.guard';
+import { Roles } from './decorators/role.decorator';
+import { Role } from '../constants/enums';
 import { UpdatePasswordDto } from './dto/UpdatePassword.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(JwtAuthGuard)
+  @Roles(Role.SuperAdmin)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Post('register/admin')
   async registerStudent(@Body() createUserDto: CreateUserDto) {
     return new User(await this.authService.createAccount(createUserDto));
