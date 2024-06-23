8abb7bc8525788277ba9d13d24d2290a5ccd22c6
Korede <adeyanju.akorede1@gmail.com>
2024-06-23 18:26:41 +0100

    fix: reduce server memory usage

--- src/sigma-quiz/services/sigma-quiz.service.ts
+++ src/sigma-quiz/services/sigma-quiz.service.ts
@@ -179,12 +179,6 @@ export class SigmaQuizService {
     const quiz = await this.findOneById(quizId, {
       schoolRegistrations: {
         rounds: {
-          round: {
-            questions: {
-              answered_by: true,
-              bonus_to: true
-            }
-          },
           answered_questions: true,
           bonus_questions: true,
         },
